484b4db481eb7aa985360366d929f3ad813f6b88
Nicole M <nicmatts@gmail.com>
2014-12-15 09:46:09 -0600

    Add Game controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,8 +1,21 @@
 class GameController < ApplicationController
   def index
+    @games = Game.all
   end
 
   def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.create(game_params)
+
+    if @game.save
+      flash[:notice] = "#{@game.name} was created."
+      redirect_to(:action => "index")
+    else
+      render("new")
+    end
   end
 
   def show
@@ -10,4 +23,9 @@ class GameController < ApplicationController
 
   def delete
   end
+
+   private
+    def game_params
+      params.require(:game).permit(:name)
+    end
 end
