c4b3dd9d429caf1ec5627f6f5782e91953d8bb25
Nicole M <nicmatts@gmail.com>
2014-12-16 12:23:02 -0600

    Add foreign key to Player table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216165452) do
+ActiveRecord::Schema.define(version: 20141216181547) do
 
   create_table "games", force: true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20141216165452) do
     t.integer  "turn_order"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "game_id"
   end
 
 end
