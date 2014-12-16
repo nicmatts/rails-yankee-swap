ecfb435078b3c7ee1f6ec2bedb4fdb60a11cc22f
Nicole M <nicmatts@gmail.com>
2014-12-16 08:26:31 -0600

    Add Player CRUD

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,14 +1,29 @@
 class PlayerController < ApplicationController
 
   attr_accessor :name, :turn, :order
-  
+
   def index
+    @players = Player.order("order ASC, name ASC")
   end
 
   def show
+
   end
 
   def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.create(player_params)
+
+    if @player.save
+      @player.order = Random.rand(1...1000000)
+      flash[:notice] = "#{@player.name} was created."
+      redirect_to(:action => "index")
+    else
+      render("new")
+    end
   end
 
   def edit
@@ -16,4 +31,9 @@ class PlayerController < ApplicationController
 
   def delete
   end
+
+  private
+    def player_params
+      params.require(:player).permit(:name, :turn, :order)
+    end
 end
