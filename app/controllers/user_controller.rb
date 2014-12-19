5088633ad9b96751c4d87a1fe062cd701ac51fd2
Nicole M <nicmatts@gmail.com>
2014-12-19 08:51:22 -0600

    Correct login controller and add form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,26 +13,26 @@ class UserController < ApplicationController
   end
 
   def attempt_login
-    if params[:username].present? && params[:password].present?
-      found_user = User.where(:username => params[:username]).first
+    if params[:name].present? && params[:password].present?
+      found_user = User.where(:name => params[:name]).first
       if found_user
         authorized_user = found_user.authenticate(params[:password])
       end
     end
     if authorized_user
       session[:user_id] = authorized_user.id
-      session[:username] = authorized_user.username
+      session[:name] = authorized_user.name
       flash[:notice] = "You are now logged in."
       redirect_to(:controller =>"gift", :action => "index")
     else
-      flash[:notice] = "Invalid username/password combination."
+      flash[:notice] = "Invalid name/password combination."
       redirect_to(:action => "login")
     end
   end
 
   def logout
     session[:user_id] = nil
-    session[:username] = nil
+    session[:name] = nil
     flash[:notice] = "You are now logged out."
     redirect_to(:action => "login")
   end
