574ac8c194c72c71a0d2ba98c25ae74299439e9e
Nicole M <nicmatts@gmail.com>
2014-12-20 15:02:19 -0600

    Update CSS for turns

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,6 @@ class Game < ActiveRecord::Base
 
   validates :name, uniqueness: true
   has_many :players
+  has_many :gifts
 
 end
--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,5 +1,6 @@
 class Gift < ActiveRecord::Base
 
   belongs_to :player
+  belongs_to :game
 
 end
